7bf60a07340b2cbb22c87a515635c4bcc1af07ae
Will Feighner <read4ever2@gmail.com>
2020-10-25 09:55:51 -0500

    Initial commit

---
+++ src/Shape.java
@@ -0,0 +1,16 @@
+/*
+ * Filename: Shape.java
+ * Author: Will Feighner
+ * Date: 2020 10 25
+ * Purpose:
+ */
+
+// import statements
+
+public abstract class Shape {
+  int numberDimensions;
+
+  public static void main(String[] args) {
+
+  }
+}
---
+++ src/TwoDimensionalShape.java
@@ -0,0 +1,16 @@
+/*
+ * Filename: TwoDimensionalShape.java
+ * Author: Will Feighner
+ * Date: 2020 10 25
+ * Purpose:
+ */
+
+// import statements
+
+public class TwoDimensionalShape extends Shape {
+  double area;
+  class Circle{
+    double radius;
+    
+  }
+}
